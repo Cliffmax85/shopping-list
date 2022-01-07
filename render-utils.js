624ddab4a3aea40555166b43133c2c9fcbee0ca2
cliffmax85 <cliffmax85@gmail.com>
2022-01-06 19:20:41 -0800

    first commit

---
+++ render-utils.js
@@ -0,0 +1,16 @@
+export function renderItem(item) {
+    const itemDiv = document.createElement('div');
+    const itemP = document.createElement('p');
+
+    if (item.bought) {
+        itemDiv.classList.add('bought', 'item');
+
+    } else {
+        itemDiv.classList.add('unbought', 'item');
+    }
+
+    itemP.textContent = `${item.item} ${item.quantity}`;
+
+    itemDiv.append(itemP);
+    return itemDiv;
+}
---
+++ shopping/shopping.js
@@ -0,0 +1,56 @@
+import { 
+    checkAuth,
+    logout,
+    createItem,
+    deleteAllItems,
+    getItems,
+    buyItem
+     
+} from '../fetch-utils.js';
+import { renderItem } from '../render-utils.js';
+
+checkAuth();
+
+const itemFrom = document.querySelector('.shopping-form');
+const deleteButton = document.querySelector('.delete-button');
+const itemsEl = document.querySelector('.items');
+
+
+const logoutButton = document.getElementById('logout');
+
+itemFrom.addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+    const data = new FormData(itemFrom);
+    const item = data.get('item');
+    const quantity = data.get('quantity');
+    console.log(createItem);
+
+    await createItem(item, quantity);
+    displayItems();
+    itemFrom.reset();
+
+
+});
+
+async function displayItems() {
+    const list = await getItems();
+    console.log(typeof list);
+
+    //itemsEl.textContent = '';
+
+    for (let item of list) { 
+        const itemEl = renderItem(item);
+        itemEl.addEventListener('click', async() => {
+            await buyItem(item.id);
+            await displayItems();
+        });
+
+    }
+    itemsEl.append('itemEl');
+
+}
+
+logoutButton.addEventListener('click', () => {
+    logout();
+});
